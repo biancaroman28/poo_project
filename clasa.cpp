bed48727a402d34e57468e9b01de90d07f28fe27
biancaroman28 <biancaroman28@users.noreply.github.com>
2023-12-03 22:20:37 +0200

    tema

---
+++ clasa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by win10 on 12/3/2023.
+//
+
+#include "clasa.h"
---
+++ clasa.h
@@ -0,0 +1,14 @@
+//
+// Created by win10 on 12/3/2023.
+//
+
+#ifndef TEMA_POO_CLASA_H
+#define TEMA_POO_CLASA_H
+
+
+class clasa {
+
+};
+
+
+#endif //TEMA_POO_CLASA_H
